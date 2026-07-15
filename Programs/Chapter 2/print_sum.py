3fcbe053939f2ad3becd8b91b9eb2b0d43305cf0
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-15 21:42:43 +0530

    sum file added

---
+++ Programs/Chapter 2/print_sum.py
@@ -0,0 +1,6 @@
+# input 2 number and print sum
+# int can be used to convert string to integer with input funcion and direct with variable also
+input1 = int(input("Enter first number: "))
+input2 = input("Enter second number: ")
+sum = input1 + int(input2)
+print("Sum of", input1, "and", input2, "is:", sum)
