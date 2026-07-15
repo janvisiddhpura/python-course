396bd035a967804534cac90e1d6b000c9211ab5b
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-15 21:20:10 +0530

    keywords file added

---
+++ Programs/Chapter 2/keywords.py
@@ -0,0 +1,12 @@
+sweet = "Gulab Jamun"
+# for = "Cricket"
+# we can't use the reserved keywords as variable names. The above line will give an error if we uncomment it.
+"""
+and as assert break class
+continue def del elif else
+except False finally for from
+global if import in is
+lambda None nonlocal not or
+pass raise return True try
+while with yield
+"""
