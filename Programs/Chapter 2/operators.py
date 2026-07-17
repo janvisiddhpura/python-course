70077a6138e4f76a0972470dfc0b73808fdc0c30
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-17 20:25:26 +0530

    operators added

---
+++ Programs/Chapter 2/operators.py
@@ -0,0 +1,44 @@
+a = 12
+b = 3
+
+# arithmetic operators
+
+print("Arithmetic Operators:")
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)
+
+# comparison operators
+
+print("Comparison Operators:")
+print("a == b =", a == b)
+print("a != b =", a != b)
+print("a > b =", a > b)
+print("a < b =", a < b)
+print("a >= b =", a >= b)
+print("a <= b =", a <=  b)
+
+# logical operators
+
+print("Logical Operators:")
+print("a and b =", a and b)
+print("a or b =", a or b)
+print("not a =", not a)
+print("a > b and a < b =", a > b and a < b)
+print("a < b or a > b =", a < b or a > b)
+print("not (a > b) =", not (a > b))
+
+# assignment operators
+
+a = 12
+b = 3
+
+a = a+6
+a+= 6
+
+print("Assignment Operators:") 
+print("a = a + 6 =", a)
+print("a += 6 =", a)
