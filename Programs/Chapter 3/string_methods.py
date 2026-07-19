3d84a6528657ca816d28f44d3e4689a267be7ae9
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-19 20:29:20 +0530

    string_methods added

---
+++ Programs/Chapter 3/string_methods.py
@@ -0,0 +1,14 @@
+str = "hElLo, woRlD!"
+
+print("Original String:", str)
+print("Uppercase:", str.upper())
+print("Lowercase:", str.lower())
+print("Title Case:", str.title())   
+print("Find 'o' in str:", str.find('o'))
+print("Count of 'l' in str:", str.count('l'))
+print("Replace 'woRlD' with 'Python':", str.replace('woRlD', 'Python'))
+print("Split str by comma:", str.split(','))
+print("Replace spaces, comma with underscores:", str.replace(', ', '_'))
+
+print("Hello \n World!")
+print("Hello \t World!")
