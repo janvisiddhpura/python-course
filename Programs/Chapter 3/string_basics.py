80dc294e8425be0a035c007f12020aa8c2e5b724
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-19 19:25:18 +0530

    string_basics_operation added

---
+++ Programs/Chapter 3/string_basics.py
@@ -0,0 +1,20 @@
+str1 = 'Hello, World!'
+str2 = "Python is fun."
+str3 = '''This is a multi-line string.'''
+
+# string types
+
+print("single-line string:", str1)
+print("double-line string:", str2)
+print("multi-line string:", str3)
+
+# string concatenation
+print("Concatenated string:", str1 + " " + str2)
+
+# length of string
+print("Length  of str1:", len(str1))
+
+# strings slicing
+print("First 5 characters of str1:", str1[0:5])
+print("First 6 characters of str1:", str1[:6])
+print("from 6th index to end of str1:", str1[5:])
