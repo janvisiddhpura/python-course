5bdab397eed2774b259a8853252a581ce6c4715e
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-20 19:57:13 +0530

    language_set added

---
+++ Programs/Chapter 5/language_set.py
@@ -0,0 +1,11 @@
+# programming languages are presents:
+# ["Python", "Java", "C++","Python", "Java", "C"]
+# convert it into a set and print
+
+programmingList = ["Python", "Java", "C++","Python", "Java", "C"]
+print(type(programmingList))
+
+# convert a list into set
+programmingSet = set(programmingList)
+print(type(programmingSet))
+print(programmingSet)
