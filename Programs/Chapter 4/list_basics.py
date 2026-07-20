05e209906a58c915f8a717fb3c31e9986e8bb863
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-20 15:27:55 +0530

    list_basics added

---
+++ Programs/Chapter 4/list_basics.py
@@ -0,0 +1,10 @@
+# lists
+
+food = ["pizza", "burger", "pasta", "mango", "ice cream"]
+
+print(len(food))  # length of list
+
+print("First element:", food[0])  # first element
+print("4th element:", food[3])  # 4th element
+print("Last element:", food[-1])  # last element
+print("First element using negative index:", food[-5])  # first element using negative index
