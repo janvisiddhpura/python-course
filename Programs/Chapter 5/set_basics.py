4b805020aaa8aaee3096347440db693c25a7504e
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-20 19:18:46 +0530

    set_basicsy added

---
+++ Programs/Chapter 5/set_basics.py
@@ -0,0 +1,17 @@
+# set
+# doesn't entertain the duplicate value
+fruits = {"apple", "banana", "orange", "kiwi", "mango", "apple"}
+
+# check type
+print(type(fruits))
+
+# print set
+print(fruits)
+
+# create an empty set
+empty_set = set()
+print(type(empty_set))
+
+# add an element
+fruits.add("chickoo")
+print(fruits)
