5446fa9536d497f11001501f7e7297d51730290c
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-21 19:11:35 +0530

    language_set updated

--- Programs/Chapter 5/language_set.py
+++ Programs/Chapter 5/language_set.py
@@ -4,8 +4,10 @@
 
 programmingList = ["Python", "Java", "C++","Python", "Java", "C"]
 print(type(programmingList))
+print(programmingList)
 
 # convert a list into set
+# set will not allow duplicate values
 programmingSet = set(programmingList)
 print(type(programmingSet))
 print(programmingSet)
