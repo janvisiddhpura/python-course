d5db41e7f8c1baee2294fef0f35849cfb12b88a9
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-21 20:02:07 +0530

    loop_basics updated

---
+++ Programs/Chapter 6/loop_basics.py
@@ -0,0 +1,19 @@
+# while loop and for loop
+# print the name 100 times
+num = 1
+while num<=100:
+    print("Number exists")
+    num += 1
+print("Outside from the while loop")
+
+'''
+1 Number exists
+2 Number exists
+3 Number exists
+4 Number exists
+5 Number exists
+.
+.
+.
+100 Number exists
+'''
