d2902652c06f733d1ebc625a488295af42fb4a38
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-21 20:09:07 +0530

    numbers added

---
+++ Programs/Chapter 6/show_numbers.py
@@ -0,0 +1,16 @@
+# print number from 1 to 50 using while loop
+
+i = 1
+while i <= 50:
+    print(i)
+    i += 1
+
+
+print("\n")
+
+# print number from 10 to 1 using while loop
+
+j = 10
+while j >= 1:
+    print(j)
+    j -= 1
