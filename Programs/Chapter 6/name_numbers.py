d0d70a32a5b91fd74aafe7e5c168e35508c6dfa2
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-22 18:36:51 +0530

    dynamic_numbers_with_name added

---
+++ Programs/Chapter 6/name_numbers.py
@@ -0,0 +1,11 @@
+# print name with the numbers
+# 1. Hello, Python!
+# 2. Hello, Python!
+# 3. Hello, Python!
+# 4. Hello, Python!
+# 5. Hello, Python!
+
+n = 1
+while n <= 5:
+    print(f"{n}. Hello, Python!")
+    n += 1
