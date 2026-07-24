9b32aa3dcdce25175d9b4336694a2480e4d92709
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-24 18:20:32 +0530

    welcome_greet added

---
+++ Programs/Chapter 7/welcome_greet.py
@@ -0,0 +1,11 @@
+# a function named welcome_message() that prints “Welcome to Python Programming!” three times.
+
+def welcome_message():
+    print("Welcome to Python Programming!")
+
+welcome_message()
+print("1st call")
+welcome_message()
+print("2nd call")
+welcome_message()
+print("3rd call")
