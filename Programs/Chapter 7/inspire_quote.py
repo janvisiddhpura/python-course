3c91d80b086dad2f3b99bc6fc480d0418d2df53d
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-24 18:53:08 +0530

    inspire_quotes added

---
+++ Programs/Chapter 7/inspire_quote.py
@@ -0,0 +1,7 @@
+# a function inspire() that prints a motivational quote with your name.
+
+def inspire():
+    print("You have the right to work, but never to the fruit of work.")
+    print("Perform your obligatory duty, because action is indeed better than inaction.")
+
+inspire()
