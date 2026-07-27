178cf8df17bc12962a52764a9cdc13c68ccb11e0
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-07-27 20:39:32 +0530

    write_mode added

--- Programs/Chapter 8/write_mode.py
+++ Programs/Chapter 8/write_mode.py
@@ -1,4 +1,6 @@
 # Open a file called report.txt in write mode.
 # ifthe files doesn't exist, it'll create it.
 
-file = open("report.txt", "w")
+file = open("write_mode_sample.txt", "a")
+# Use append mode if don't want to override the content of the file
+file.write("Learning Python in a detailed way! That's Great!!")
