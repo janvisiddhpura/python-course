30cdb9b8ece2b11d1ceb068f19d8f79e9ceffa5a
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-08-01 19:07:22 +0530

    print_multiple_names added

--- Programs/1_Introduction_to_Python/print_multiple_names.py
+++ Programs/1_Introduction_to_Python/print_multiple_names.py
@@ -1,2 +1,2 @@
-# Print name 10 times using * symbol and each time it should be on next line.
+# Person name 10 times using * symbol and each time it should be on next line.
 print(("William Johnson" + "\n" ) * 10)
