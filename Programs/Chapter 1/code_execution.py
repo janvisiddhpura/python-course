bdb30d903ff1feec837213c8425a5ece635e3130
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-08-01 19:11:20 +0530

    code_execution_sequence added

--- Programs/Chapter 1/code_execution.py
+++ Programs/Chapter 1/code_execution.py
@@ -1,7 +1,8 @@
-name = "Janvi Siddhpura"
+name = "Jack Johnson"
 age1 = 23
 age2 = 24
 print("Actual value: ", age2)
 favSubject = "Python"
 age2 = 26
+print("Fav Subject: ", favSubject)
 print("Updated value: ", age2)
